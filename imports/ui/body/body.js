05a1546cf723f5c07ab2efacb2b95618073cda6d
jason tlh <jasonjdh94@gmail.com>
2020-03-09 21:26:25 -0400

    completed sort functionality on overview

--- imports/ui/body/body.js
+++ imports/ui/body/body.js
@@ -4,3 +4,4 @@ import './body.scss';
 import '../components/navbar/navbar.js';
 import '../components/search/search.js';
 import '../components/overview/overview.js';
+import '../components/comments/comments.js';
--- imports/ui/components/overview/overview.js
+++ imports/ui/components/overview/overview.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Data } from '../../../api/data.js';
 
@@ -8,11 +9,17 @@ import './overview.scss';
 import '../room/room.js';
 
 Template.overview.onCreated(function bodyOnCreated() {
+  // setting state
+  this.state = new ReactiveDict();
+  // setting inital sort to room low to high
+  this.state.set('sort', 'roomDown')
+  // setting subscription to the collection on the server
   Meteor.subscribe('data');
 });
 
 Template.overview.helpers({
   data() {
+    // retrieve data from collection
     const rooms = Data.find({ room: { $ne: "null" } }).fetch()
     // tried to filter all rooms using $or but it kept returning every document, could other wise skip a step 
     // const rooms = Data.find({ $or: [{ room: { $ne: "null" } }, { room: { $ne: "" } }] }).fetch()
@@ -51,17 +58,85 @@ Template.overview.helpers({
       value.rating.forEach(num => {
         avg += Number(num)
       })
-      stats[value.room].average = Math.floor(avg / value.rating.length)
+      let result = Math.floor(avg / value.rating.length)
+      stats[value.room].average = result
+
+      // applying color to rating
+      if (Number(result) <= 6) {
+        stats[value.room].red = "red"
+      } else if (Number(result) >= 9) {
+        stats[value.room].green = "green"
+      } else {
+        stats[value.room].yellow = "yellow"
+      }
 
       // trimming date information, potentially change to regex
       const trim = value.ratingDate[value.ratingDate.length - 1]
-      .split('')
-      .slice(4, 15)
-      .join('')
-      
+        .split('')
+        .slice(4, 15)
+        .join('')
+
       stats[value.room].lastModified = trim
     })
 
-    return objectValues
+    const instance = Template.instance()
+    // sort functions when appropriate elements are clicked
+    if (instance.state.get('sort') === "roomDown") {
+      objectValues.sort((a, b) => Number(a.room) < Number(b.room) ? -1 : 1)
+      return objectValues
+    } else if (instance.state.get('sort') === "roomUp") {
+      objectValues.sort((a, b) => Number(a.room) > Number(b.room) ? -1 : 1)
+      return objectValues
+    } else if (instance.state.get('sort') === "frequencyDown") {
+      objectValues.sort((a, b) => a.rating.length > b.rating.length ? -1 : 1)
+      return objectValues
+    } else if (instance.state.get('sort') === "frequencyUp") {
+      objectValues.sort((a, b) => a.rating.length < b.rating.length ? -1 : 1)
+      return objectValues
+    } else if (instance.state.get('sort') === "percentageDown") {
+      objectValues.sort((a, b) => a.dissatisfied > b.dissatisfied ? -1 : 1)
+      return objectValues
+    } else if (instance.state.get('sort') === "percentageUp") {
+      objectValues.sort((a, b) => a.dissatisfied < b.dissatisfied ? -1 : 1)
+      return objectValues
+    } else if (instance.state.get('sort') === "averageDown") {
+      objectValues.sort((a, b) => a.average > b.average ? -1 : 1)
+      return objectValues
+    } else if (instance.state.get('sort') === "averageUp") {
+      objectValues.sort((a, b) => a.average < b.average ? -1 : 1)
+      return objectValues
+    }
   }
 });
+
+Template.overview.events({
+  // setting state to sort columns both ways, high to low and low to high
+  'click .sort-room'(event, instance) {
+    if (instance.state.get('sort') === 'roomDown') {
+      instance.state.set('sort', 'roomUp')
+    } else {
+      instance.state.set('sort', 'roomDown')
+    }
+  },
+  'click .sort-frequency'(event, instance) {
+    if (instance.state.get('sort') === 'frequencyDown') {
+      instance.state.set('sort', 'frequencyUp')
+    } else {
+      instance.state.set('sort', 'frequencyDown')
+    }
+  },
+  'click .sort-percentage'(event, instance) {
+    if (instance.state.get('sort') === 'percentageDown') {
+      instance.state.set('sort', 'percentageUp')
+    } else {
+      instance.state.set('sort', 'percentageDown')
+    }
+  },
+  'click .sort-average'(event, instance) {
+    if (instance.state.get('sort') === 'averageDown') {
+      instance.state.set('sort', 'averageUp')
+    } else {
+      instance.state.set('sort', 'averageDown')
+    }
+  },
+})
